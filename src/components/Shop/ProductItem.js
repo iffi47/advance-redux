8b75a4f495f4d6f3ded03e6c8ee7aea241430383
iffi47 <iftikharlion958@gmail.com>
2026-07-06 22:41:13 +0500

    refactor ProductItem and Products components to integrate cart functionality and improve product listing

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,22 +1,34 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
 
+  const addItemToCart = (event) => {
+   dispatch(
+    cartActions.addItemToCart({
+     id,
+     title,
+     price,
+    }),
+   );
+  };
   return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
+   <li className={classes.item}>
+    <Card>
+     <header>
+      <h3>{title}</h3>
+      <div className={classes.price}>${price.toFixed(2)}</div>
+     </header>
+     <p>{description}</p>
+     <div className={classes.actions}>
+      <button onClick={addItemToCart}>Add to Cart</button>
+     </div>
+    </Card>
+   </li>
   );
 };
 
