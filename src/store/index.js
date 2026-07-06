9184da5dba68212ac061dd5b20e9af7816ca0e5f
iffi47 <iftikharlion958@gmail.com>
2026-07-06 17:06:04 +0500

    add react-redux integration with ui slice and store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./cart";
+import authReducer from "./auth";
+import uiReducer from "./uiSlice";
 
 const store = configureStore({
-  reducer:{
-    cart: cartReducer
-  },
+ reducer: {
+  cart: cartReducer,
+  auth: authReducer,
+  ui: uiReducer,
+ },
 });
 
 export default store;
