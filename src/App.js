571f01df17e77b3a73661583255724e439fe52e7
iffi47 <iftikharlion958@gmail.com>
2026-07-07 18:20:54 +0500

    feat: add firebase database

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
 
 function App() {
-  const toggleCart = useSelector((state) => state.ui.cartIsVisible);
-  return (
-    <Layout>
-     {toggleCart && <Cart />}
-      <Products />
-    </Layout>
+ const toggleCart = useSelector((state) => state.ui.cartIsVisible);
+ const cart = useSelector((state) => state.cart);
+ useEffect(() => {
+  fetch(
+   "https://react-practise-417a1-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+   { method: "PUT", body: JSON.stringify(cart) },
   );
+ }, [cart]);
+ return (
+  <Layout>
+   {toggleCart && <Cart />}
+   <Products />
+  </Layout>
+ );
 }
 
 export default App;
